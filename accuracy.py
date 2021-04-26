3567f09811ba75628f1638b7186329a71b00ab99
Ryan Folz <folz.ryan@yahoo.com>
2021-04-26 18:49:02 -0400

    Add files via upload

--- accuracy.py
+++ accuracy.py
@@ -9,64 +9,129 @@ in order to run this script (more specifically selenium) you must download the c
 if you do not have it you can download it from here:
 https://chromedriver.chromium.org/downloads
 '''
-
-"""
-Set the difficuly/grid size
-NOTE: 10x10 Easy and 15x15 Easy can take a while to finish running. We were unable to run through a large number of
-puzzles because occasionally you would run into boards that have very large aquariums and computing the permutations
-could take a long time. 
-0 = 6x6 Easy
-1 = 6x6 Normal
-2 = 6x6 Hard
-3 = 10x10 Easy
-4 = 10x10 Normal
-5 = 10x10 Hard
-6 = 15x15 Easy
-7 = 15x15 Normal
-8 = 15x15 Hard
-"""
-setting = 10
-
 PATH = "D:/Rando/chromedriver_win32/chromedriver.exe"
-TOTAL_RUNS = 500
 
-driver = webdriver.Chrome(PATH)
+"""
 
-for setting in range(7, 8):
-    total_constraint_time = 0
-    total_generate_solution_time = 0
-    total_successes = 0
-    total_failures = 0
-    url = f"https://www.puzzle-aquarium.com/?size={setting}"
+"""
 
-    if setting < 3:
-        GRID_SIZE = 6
-        SIZE = 25
-    elif setting < 6:
-        GRID_SIZE = 10
-        SIZE = 41
-    else:
-        GRID_SIZE = 15
-        SIZE = 61
 
-    for j in range(TOTAL_RUNS):
-        driver.get(url)
-        html = driver.page_source
-        soup = bs(html, "html.parser")
+class WebTest:
+    """
+    This class is responsible for taking in a list of settings and trying out our AquariumSolver on a website. It uses
+    chromedriver to open the website, scrape the board, generate a solution, enter to solution on the website, and then
+    see if the website likes the answer.
+    """
+    def __init__(self, settings):
+        """
+        This constructor expects an input list of tuples that specifies which board to run and for how many attempts.
+        For example, an input list of [(0, 10), (8, 5)] would run against the 6x6 Easy 10 times and the 15x15 Hard 8
+        times.
 
-        # get the column numbers
+        Set the difficulty/grid size
+        NOTE: 10x10 Easy and 15x15 Easy can take a while to finish running. We were unable to run through a large number
+        of puzzles because occasionally you would run into boards that have very large aquariums and computing the
+        permutations could take a long time.
+        0 = 6x6 Easy
+        1 = 6x6 Normal
+        2 = 6x6 Hard
+        3 = 10x10 Easy
+        4 = 10x10 Normal
+        5 = 10x10 Hard
+        6 = 15x15 Easy
+        7 = 15x15 Normal
+        8 = 15x15 Hard
+        :param settings: A list of tuples telling the class which board to try and for how many runs.
+        """
+        self.__settings = settings
+
+    def run(self):
+        """
+        Run the accuracy test.
+        :return:
+        """
+        for setting, total_runs in self.__settings:
+            self.__reset_aggregation_values()
+            self.__update_sizes(setting)
+            self.__update_url(setting)
+            self.__run_webdriver(total_runs)
+            self.__print_results()
+
+    def __reset_aggregation_values(self):
+        self.__total_constraint_time = 0
+        self.__total_generate_solution_time = 0
+        self.__total_successes = 0
+        self.__total_failures = 0
+
+    def __update_sizes(self, setting):
+        self.__current_setting = setting
+        if setting < 3:
+            self.__grid_size = 6
+            self.__size = 25
+        elif setting < 6:
+            self.__grid_size = 10
+            self.__size = 41
+        else:
+            self.__grid_size = 15
+            self.__size = 61
+
+    def __update_url(self, setting):
+        self.__url = f"https://www.puzzle-aquarium.com/?size={setting}"
+
+    def __run_webdriver(self, total_runs):
+        driver = webdriver.Chrome(PATH)
+        for _ in range(total_runs):
+            driver.get(self.__url)
+            html = driver.page_source
+            soup = bs(html, "html.parser")
+
+            top_numbers = self.__get_top_numbers(soup)
+            side_numbers = self.__get_side_numbers(soup)
+            board = self.__get_board(soup)
+
+            solver = AquariumSolver(board, top_numbers, side_numbers)
+
+            solution, constraint_time, generate_solution_time = solver.solve()
+
+            # click on cells on website
+            id = 2
+            for row in solution:
+                for num in row:
+                    if num > 0:
+                        cell = driver.find_element_by_xpath(f"//*[@id=\"game\"]/div[{self.__size}]/div[{id}]")
+                        cell.click()
+                    id += 1
+
+            # click on 'done' to verify solution
+            done = driver.find_element_by_xpath("//*[@id=\"btnReady\"]")
+            done.click()
+
+            if "Congratulations! You have solved the puzzle" in driver.page_source:
+                self.__total_constraint_time += constraint_time
+                self.__total_generate_solution_time += generate_solution_time
+                self.__total_successes += 1
+            else:
+                print("Error: Solution was not successful")
+                self.__total_failures += 1
+                driver.close()
+                driver = webdriver.Chrome(PATH)
+        driver.close()
+
+    def __get_top_numbers(self, soup):
         numbers = soup.find_all(class_="cell task v")
         top_numbers = []
         for number in numbers:
             top_numbers.append(int(number.text))
+        return top_numbers
 
-        # get the row numbers
+    def __get_side_numbers(self, soup):
         numbers = soup.find_all(class_="cell task h")
         side_numbers = []
         for number in numbers:
             side_numbers.append(int(number.text))
+        return side_numbers
 
-        # determine the sub-aquariums
+    def __get_board(self, soup):
         c = soup.find_all(tabindex="1")[1:]
         grid = []
         row = []
@@ -81,7 +146,7 @@ for setting in range(7, 8):
         (it may still have a border if it is on an edge)
         """
         for i in range(len(c)):
-            if i % GRID_SIZE == 0 and i != 0:
+            if i % self.__grid_size == 0 and i != 0:
                 grid.append(row)
                 row = []
             # split the div and extract the sides from the class
@@ -119,36 +184,18 @@ for setting in range(7, 8):
                     board[x][y] = aquarium_index
                 aquarium_index += 1
 
-        solver = AquariumSolver(board, top_numbers, side_numbers)
-        solution, constraint_time, generate_solution_time = solver.solve()
+        return board
 
-        # click on cells on website
-        id = 2
-        for row in solution:
-            for num in row:
-                if num > 0:
-                    cell = driver.find_element_by_xpath(f"//*[@id=\"game\"]/div[{SIZE}]/div[{id}]")
-                    cell.click()
-                id += 1
+    def __print_results(self):
+        print(f"Looking at setting:", self.__current_setting)
+        print(f"Total Successes: {self.__total_successes}")
+        print(f"Total Failures: {self.__total_failures}")
+        print(f"Accuracy: {self.__total_successes / (self.__total_successes + self.__total_failures)}")
+        print(f"Average constraint making time: {self.__total_constraint_time / self.__total_successes}s")
+        print(f"Average solution generation time: {self.__total_generate_solution_time / self.__total_successes}s\n")
 
-        # click on 'done' to verify solution
-        done = driver.find_element_by_xpath("//*[@id=\"btnReady\"]")
-        done.click()
 
-        if "Congratulations! You have solved the puzzle" in driver.page_source:
-            total_constraint_time += constraint_time
-            total_generate_solution_time += generate_solution_time
-            total_successes += 1
-        else:
-            print("Error: Solution was not successful")
-            total_failures += 1
-            driver.close()
-            driver = webdriver.Chrome(PATH)
-
-    print(f"Total Successes: {total_successes}")
-    print(f"Total Failures: {total_failures}")
-    print(f"Accuracy: {total_successes/(total_successes + total_failures)}")
-    print(f"Average constraint making time: {total_constraint_time / total_successes}s")
-    print(f"Average solution generation time: {total_generate_solution_time / total_successes}s")
-
-driver.close()
+if __name__ == "__main__":
+    settings = [(0, 10), (1, 10), (2, 10)]
+    accuracy = WebTest(settings)
+    accuracy.run()
